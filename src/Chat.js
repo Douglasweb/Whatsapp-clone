c19501c41cf7241a2924cc85e6c97acf0df51a1b
Douglas Eduardo Ferreira Costa <webdouglas@hotmail.com>
2021-03-09 23:32:08 -0300

    Layout feito

--- src/Chat.js
+++ src/Chat.js
@@ -1,5 +1,5 @@
 import { Avatar, IconButton } from '@material-ui/core';
-import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+import { AttachFile, InsertEmoticon, Mic, MoreVert, SearchOutlined } from '@material-ui/icons';
 import React, { useState, useEffect } from 'react';
 import './Chat.css';
 
@@ -8,12 +8,18 @@ function Chat() {
 
     const [seed, setSeed] = useState('')
 
+    const [input, setInput] = useState('')
+
     useEffect(() => {
       
         setSeed(Math.floor(Math.random() * 5000));
     
     }, [])
 
+    const SendMessage = (e) => {
+        e.preventDefault();
+        console.log('Voce escreveu >> ', input);
+    }
 
     return (
         <div className='chat'>
@@ -39,8 +45,27 @@ function Chat() {
 
                     </div>
                 </div>
-                <div className="chat__body"></div>
-                <div className="chat__footer"></div>   
+                <div className="chat__body">
+                        <p className={`chat__message ${true && `chat__reciever`}`}>
+                            <span className="chat__name">Douglas</span>
+                            Ola pessoal
+                            <span className="chat__timestamp">13:52</span>                       
+                        </p>
+                        
+                        
+                </div>
+                <div className="chat__footer">
+                    <InsertEmoticon /> 
+                    <form>
+                       <input value={input} 
+                       onChange={ (e) => setInput(e.target.value) } 
+                       placeholder="Escreva sua mensagem" 
+                       text="text"  />
+                       
+                       <button type="submit"  onClick={SendMessage}  >Send a message</button> 
+                    </form> 
+                    <Mic />
+                </div>   
         </div>
     )
 }
